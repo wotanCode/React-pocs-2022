20261571f030790bd4ac642c369d2f015a31d890
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-12-09 12:51:03 -0300

    feat/40

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,12 @@
 function ExpenseItem(){
     return (
         <div>
-            <h1>Expenseltem component</h1>
+            <div>March 28th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
         </div>
-    )
+    );
 }
 export default ExpenseItem;
