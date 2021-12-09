ae19a6ae4a4cb15fbb2f88bddfc22e7148929b5b
Pedro Yanez <pedro_y91@outlook.com>
2021-12-09 12:40:36 -0300

    feat/33 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 
+//Components
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div className="App">
       <h1>reakt</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h1>Expenseltem component</h1>
+        </div>
+    )
+}
+export default ExpenseItem;
