759f7bd3e39a5dfbb47808580645720d3e64a934
Pedro Yanez <pedro_y91@outlook.com>
2021-12-09 14:38:09 -0300

    36

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+//CSS
+import "./ExpenseItem.css";
+
 function ExpenseItem(){
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTittle = "Car Insurance";
+    const expenseAmount = 294.67
     return (
-        <div>
-            <div>March 28th 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTittle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
