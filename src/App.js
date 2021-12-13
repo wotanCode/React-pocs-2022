86d6b0c32e1c5aee0125fda504f619549cac1280
PPedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-12-13 15:18:36 -0300

    feat/37

--- src/App.js
+++ src/App.js
@@ -30,7 +30,10 @@ function App() {
   return (
     <div className="App">
       <h1>react</h1>
-      <ExpenseItem title="Tottle papel"></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
