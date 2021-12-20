4eea93716534bdb8276aae0cd63372ec287787db
Pedro Yanez <pedro_y91@outlook.com>
2021-12-20 08:34:59 -0300

    47

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,7 +5,9 @@ import ExpenseData from "./ExpenseData"
 import Card from "../UI/Card"
 
 const ExpenseItem = (props) =>{
-
+const clickhandler = () =>{
+    console.log("click!!")
+}
 
     return (
         <Card className="expense-item">
@@ -14,6 +16,7 @@ const ExpenseItem = (props) =>{
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickhandler}>Change title</button>
         </Card>
     );
 }
