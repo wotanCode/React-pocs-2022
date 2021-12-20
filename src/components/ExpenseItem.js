f1d3323456bf9f5095ede2047079d8237e5b9f05
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-12-20 07:29:31 -0300

    40

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,18 +2,19 @@
 import "./ExpenseItem.css";
 //Components
 import ExpenseData from "./ExpenseData"
+import Card from "./Card"
 
 function ExpenseItem(props){
 
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseData date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 export default ExpenseItem;
