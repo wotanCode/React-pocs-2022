beae31c9ea7f31eb41e0dc53e1a927bde24f2c7a
Pedro Yanez <pedro_y91@outlook.com>
2021-12-21 13:47:57 -0300

    fix/59 child to dad

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,13 +4,13 @@ import ExpenseForm from './ExpenseForm';
 
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
     const saveExpenseDateHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-    console.log(expenseData);
+        props.onAddExpense(expenseData)
     };
 
     return (
