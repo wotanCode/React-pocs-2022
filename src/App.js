18dd5ff3102b53a1fbb21028d6b097a170e297df
Pedro Yanez <pedro_y91@outlook.com>
2021-12-22 13:40:25 -0300

    67/mostrando de manera condicional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
@@ -34,10 +34,6 @@ const App = () => {
       return [expense, ...prevExpenses];
     });
   };
-
-useEffect(()=>{
-  console.log(expenses);
-},[expenses]);
 
   return (
     <div>
