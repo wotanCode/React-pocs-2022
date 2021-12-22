208c697d1235dd51f3509fb81997450d17dcce88
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-12-22 15:13:39 -0300

    68/aspecto Avanzado del condicional returnt

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ExpensesList.css";
+
+import ExpenseItem from './ExpenseItem.js'
+
+const ExpensesList = (props) => {
+    if (props.items.length === 0) {
+        return(
+            <h2 className="expenses-list__fallback"> Found no expenses.</h2>
+        );
+    }
+
+    return (
+        <li>
+        <ul className="expenses-list">
+            {props.items.map((expense) => (
+            <ExpenseItem
+                key= {expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+            ))}
+        </ul>      
+        </li>
+          );
+};
+export default ExpensesList;
