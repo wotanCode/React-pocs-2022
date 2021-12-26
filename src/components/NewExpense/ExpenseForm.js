d94c15411c6f76923788674a1e26e533871fdb49
Pedro Yanez <pedro_y91@outlook.com>
2021-12-26 18:09:38 -0300

    71/wrap up & next steps

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,9 +23,9 @@ const ExpenseForm = (props) => {
         event.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        };
+            amount: +enteredAmount,
+            date: new Date(enteredDate),
+          };
         props.onSaveExpenseData(expenseData);
 
         setEnteredTitle('');
